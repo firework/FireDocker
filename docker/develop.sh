52a5b4b17800f36391254598ce4174dc10762b62
Daniel Polito <contato@dbpolito.net>
2017-10-17 15:23:06 -0200

    Update develop.sh

--- docker/develop.sh
+++ docker/develop.sh
@@ -9,64 +9,59 @@ export DB_NAME=${DB_NAME:-docker}
 export DB_USER=${DB_USER:-docker}
 export DB_PASS=${DB_PASS:-secret}
 
+# Normalize basedir
+cd "$(dirname $0)"/../
+BASE_DIR=$(pwd)
+
+# Decide project name
+PROJECT_NAME=$(basename $BASE_DIR)
+
 # Decide which docker-compose file to use
 COMPOSE_FILE="dev"
 
 # Disable pseudo-TTY allocation for CI (Jenkins)
 TTY=""
 
-if [ ! -z "$BUILD_NUMBER" ]; then
-    COMPOSE_FILE="ci"
-    TTY="-T"
-fi
+# if [ ! -z "$BUILD_NUMBER" ]; then
+#     COMPOSE_FILE="ci"
+#     TTY="-T"
+# fi
 
-COMPOSE="docker-compose -f $(dirname $0)/compose.$COMPOSE_FILE.yml"
+COMPOSE="docker-compose -f $BASE_DIR/docker/compose.$COMPOSE_FILE.yml -p $PROJECT_NAME"
 
 COMMAND_RUN="run --rm $TTY"
 COMMAND_EXEC="exec"
 
-if [ $# -gt 0 ];then
-    if [ "$1" == "art" ] || [ "$1" == "artisan" ]; then
-        shift 1
-        $COMPOSE $COMMAND_EXEC \
-            app \
-            php artisan "$@"
-
-    # If "composer" is used, pass-thru to "composer"
-    # inside a new container
-    elif [ "$1" == "composer" ]; then
-        shift 1
-        $COMPOSE $COMMAND_EXEC \
-            app \
-            composer "$@"
-
-    # If "test" is used, run unit tests,
-    # pass-thru any extra arguments to php-unit
-    elif [ "$1" == "t" ] || [ "$1" == "test" ]; then
-        shift 1
-        $COMPOSE $COMMAND_EXEC \
-            app \
-            ./vendor/bin/phpunit "$@"
-
-    # If "npm" or "yarn" is used, run it
-    # from our node container
-    elif [ "$1" == "npm" ] || [ "$1" == "yarn" ]; then
-        COMMAND=$1
-        shift 1
-        $COMPOSE $COMMAND_RUN \
-            node \
-            $COMMAND "$@"
-
-    # If "gulp" is used, run gulp
-    # from our node container
-    elif [ "$1" == "gulp" ]; then
-        shift 1
-        $COMPOSE $COMMAND_RUN \
-            node \
-            ./node_modules/.bin/gulp "$@"
-    else
-        $COMPOSE "$@"
-    fi
+if [ $# -gt 0 ]; then
+    COMMAND=$1
+    shift 1
 else
-    $COMPOSE ps
+    COMMAND="ps"
 fi
+
+case "$COMMAND" in
+    "art" | "artisan")
+        $COMPOSE $COMMAND_EXEC app php artisan "$@"
+    ;;
+    "composer")
+        $COMPOSE $COMMAND_EXEC app composer "$@"
+    ;;
+    "t" | "test")
+        $COMPOSE $COMMAND_EXEC app ./vendor/bin/phpunit "$@"
+    ;;
+    "node")
+        $COMPOSE $COMMAND_RUN node "$@"
+    ;;
+    "npm" | "yarn")
+        $COMPOSE $COMMAND_RUN node $COMMAND "$@"
+    ;;
+    "exec")
+        $COMPOSE $COMMAND_EXEC "$@"
+    ;;
+    "run")
+        $COMPOSE $COMMAND_RUN "$@"
+    ;;
+    *)
+        $COMPOSE $COMMAND "$@"
+    ;;
+esac
